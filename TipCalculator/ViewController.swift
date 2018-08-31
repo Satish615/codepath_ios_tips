518607676862a2d3732bcafa6ea8921812480ba3
satish Kumar Gollaprolu <satishkumar.gollaprolu@sjsu.edu>
2018-08-30 20:47:08 -0700

    Tip calc

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FIELD: UITextField!
+    @IBOutlet weak var twentyp: UILabel!
+    @IBOutlet weak var fifp: UILabel!
+    @IBOutlet weak var tenp: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Calc(_ sender: Any) {
+        let amount:Double = Double(self.FIELD.text!)!
+        let ten:Double = amount*0.1
+        let fif:Double = amount*0.15
+        let twenty:Double = amount*0.20
+        tenp.text = String(ten)
+        fifp.text = String(fif)
+        twentyp.text = String(twenty)
+        
+    }
+    
 }
 
